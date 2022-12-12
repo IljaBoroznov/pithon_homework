d1285bd8ab9e71e5074d0d3dac1d2cfe8277717c
Илья <ilja_boroznov@mail.ru>
2022-12-12 14:19:29 +0300

    correct Task_2

--- Homework_5/Task_2.py
+++ Homework_5/Task_2.py
@@ -19,32 +19,27 @@ while count < 9:
                     a[i][j] = 0
                 print(a[i][j], end = ' ')
             print()
+        b = 0
         for i in range(len(a)):
             for j in range(len(a[i])):
                 if a[0][0] == 0 and a[0][1] == 0 and a[0][2] == 0:
-                    print(f"победа {player1} ")
-                    break
-                if a[1][0] == 0 and a[1][1] == 0 and a[1][2] == 0:
-                    print(f"победа {player1} ")
-                    break
-                if a[2][0] == 0 and a[2][1] == 0 and a[2][2] == 0:
-                    print(f"победа {player1} ")
-                    break
-                if a[0][0] == 0 and a[1][0] == 0 and a[2][0] == 0:
-                    print(f"победа {player1} ")
-                    break
-                if a[0][1] == 0 and a[1][1] == 0 and a[2][1] == 0:
-                    print(f"победа {player1} ")
-                    break
-                if a[0][2] == 0 and a[1][2] == 0 and a[2][2] == 0:
-                    print(f"победа {player1} ")
-                    break
-                if a[0][0] == 0 and a[1][1] == 0 and a[2][2] == 0:
-                    print(f"победа {player1} ")
-                    break
-                if a[0][2] == 0 and a[1][1] == 0 and a[2][0] == 0:
-                    print(f"победа {player1} ")
-                    break
+                    b = 1 
+                elif a[1][0] == 0 and a[1][1] == 0 and a[1][2] == 0:
+                    b = 1   
+                elif a[2][0] == 0 and a[2][1] == 0 and a[2][2] == 0:
+                    b = 1    
+                elif a[0][0] == 0 and a[1][0] == 0 and a[2][0] == 0:
+                    b = 1   
+                elif a[0][1] == 0 and a[1][1] == 0 and a[2][1] == 0:
+                    b = 1    
+                elif a[0][2] == 0 and a[1][2] == 0 and a[2][2] == 0:
+                    b = 1    
+                elif a[0][0] == 0 and a[1][1] == 0 and a[2][2] == 0:
+                    b = 1   
+                elif a[0][2] == 0 and a[1][1] == 0 and a[2][0] == 0:
+                    b = 1
+        if b == 1:
+            print(f"победа {player1} ")
         count += 1
     else:
         print(f'{player2} введите номер позиции')
@@ -55,33 +50,31 @@ while count < 9:
                     a[i][j] = 'x'
                 print(a[i][j], end = ' ')
             print()
+        c = 0
         for i in range(len(a)):
             for j in range(len(a[i])):
                 if a[0][0] == 'x' and a[0][1] == 'x' and a[0][2] == 'x':
-                    print(f"победа {player2} ")
-                    break
-                if a[1][0] == 'x' and a[1][1] == 'x' and a[1][2] == 'x':
-                    print(f"победа {player2} ")
-                    break
-                if a[2][0] == 'x' and a[2][1] == 'x' and a[2][2] == 'x':
-                    print(f"победа {player2} ")
-                    break
-                if a[0][0] == 'x' and a[1][0] == 'x' and a[2][0] == 'x':
-                    print(f"победа {player2} ")
-                    break
-                if a[0][1] == 'x' and a[1][1] == 'x' and a[2][1] == 'x':
-                    print(f"победа {player2} ")
-                    break
-                if a[0][2] == 'x' and a[1][2] == 'x' and a[2][2] == 'x':
-                    print(f"победа {player2} ")
-                    break
-                if a[0][0] == 'x' and a[1][1] == 'x' and a[2][2] == 'x':
-                    print(f"победа {player2} ")
-                    break
-                if a[0][2] == 'x' and a[1][1] == 'x' and a[2][0] == 'x':
-                    print(f"победа {player2} ")
-                    break
+                    c = 1
+                elif a[1][0] == 'x' and a[1][1] == 'x' and a[1][2] == 'x':
+                    c = 1
+                elif a[2][0] == 'x' and a[2][1] == 'x' and a[2][2] == 'x':
+                    c = 1    
+                elif a[0][0] == 'x' and a[1][0] == 'x' and a[2][0] == 'x':
+                    c = 1    
+                elif a[0][1] == 'x' and a[1][1] == 'x' and a[2][1] == 'x':
+                    c = 1    
+                elif a[0][2] == 'x' and a[1][2] == 'x' and a[2][2] == 'x':
+                    c = 1    
+                elif a[0][0] == 'x' and a[1][1] == 'x' and a[2][2] == 'x':
+                    c = 1    
+                elif a[0][2] == 'x' and a[1][1] == 'x' and a[2][0] == 'x':
+                    c = 1
+        if c == 1:
+            print(f"победа {player2} ")     
         count += 1
+    if count == 9 and c != 1 and b != 1:
+        print('Ничья')
+
 
 
 
